05606e2676b016168b6aa559cf336f37d27b0f6d
Jesse <Jesseokeya@gmail.com>
2018-04-10 17:01:26 -0400

    tixi0001

--- Assign4/StringArray_test.go
+++ Assign4/StringArray_test.go
@@ -2,6 +2,41 @@ package main
 
 import "testing"
 
-func TestMain(t *testing.T) {
-	main()
+var (
+	sa = StringArray{0, 10, []string{}}
+)
+
+func init() {
+	sa.NewStringArray(&StringArray{})
+}
+
+func TestAdd(t *testing.T) {
+	allCourses := []string{
+		"CST8101",
+		"CST8110",
+		"CST8215",
+		"CST8300",
+		"MAT8001",
+		"empty",
+	}
+
+	for _, course := range allCourses {
+		sa.Add(course)
+	}
+	if sa.IsEmpty() {
+		t.Error("String Array Object Should Not Be Empty ")
+	}
+}
+
+func TestAddAt(t *testing.T) {
+	capacity := sa.Capacity()
+	sa = StringArray{10, capacity, sa.stringArray}
+	sa.AddAt(29, "CST8108")
+	if sa.IsEmpty() {
+		t.Error("String Array Object Should Not Be Empty ")
+	}
+}
+
+func TestClear(i *testing.T) {
+	sa.clear()
 }
